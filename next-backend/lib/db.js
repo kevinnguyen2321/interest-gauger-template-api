00dbc016b15ee49a69a3f1e0c5f9e14cd3667368
Kevin Nguyen <kevin.nguyen2321@yahoo.com>
2025-02-10 16:47:44 -0600

    Updated connection string on db.js to reflect new cloud db

--- next-backend/lib/db.js
+++ next-backend/lib/db.js
@@ -1,12 +1,19 @@
 import { Pool } from 'pg';
 
 // Database connection credentials
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Required for Neon
+  },
 });
 
 export default pool;
