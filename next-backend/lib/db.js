ec97b18ad1b1786d40eac143843ae402cf238e86
Kevin Nguyen <kevin.nguyen2321@yahoo.com>
2025-02-10 16:53:35 -0600

    Removed comments on db.js

--- next-backend/lib/db.js
+++ next-backend/lib/db.js
@@ -1,14 +1,5 @@
 import { Pool } from 'pg';
 
-// Database connection credentials
-// const pool = new Pool({
-//   user: process.env.DB_USER,
-//   host: process.env.DB_HOST,
-//   database: process.env.DB_NAME,
-//   password: process.env.DB_PASSWORD,
-//   port: process.env.DB_PORT,
-// });
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
